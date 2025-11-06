42c17942be70e70520c2c4cd47d89aab30dc72db
KAIQUE DOS SANTOS SOUSA <kaique.ssousa3@senacsp.edu.br>
2025-11-06 14:42:05 -0300

    aula06-11

---
+++ operadoreslogicos/Program.cs
@@ -0,0 +1,15 @@
+﻿// Operadores Lógicos
+using System;
+
+bool x = true;
+bool y = false;
+
+bool and = x  && y; // E Lógico
+bool or = x || y;
+bool not = !x;
+
+// Exibindo resultados dos operadores lógicos
+Console.WriteLine("\nOperadores Lógicos:");
+Console.WriteLine($"x && y: {and}"); // E Lógico
+Console.WriteLine($"x || y: {or}"); // Não Lógico
+Console.WriteLine($"!x: {not}"); // Não Lógico
---
+++ operadoresrelacionais/Program.cs
@@ -0,0 +1,20 @@
+﻿int a = 10;
+int b = 5;
+
+//Operadoresrelacioanais
+bool maiorque = a > b;
+bool menorque = a < b;
+bool igual = a ==b;
+bool diferetente  = a != b;
+bool maiorOuigual = a >= b;
+bool menorOuigual = a <= b;
+
+//exibindo resultado dos opreadores relacionais
+Console.WriteLine("\nOperadores Relacionais");
+Console.WriteLine($"{a} > {b} : {maiorque}");
+Console.WriteLine($"{a} < {b} : {menorque}");
+Console.WriteLine($"{a} == {b} : {igual}");
+Console.WriteLine($"{a} != {b} : {diferetente}");
+Console.WriteLine($"{a} >= {b} : {maiorOuigual}");
+Console.WriteLine($"{a} <= {b} : {menorOuigual}");
+
