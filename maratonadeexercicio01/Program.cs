c541e6d91bf7c38450eef0086fd6c9932d3a4e27
KAIQUE DOS SANTOS SOUSA <kaique.ssousa3@senacsp.edu.br>
2025-11-11 14:41:14 -0300

    primeiro commit

---
+++ maratonadeexercicio01/Program.cs
@@ -0,0 +1,66 @@
+﻿/*1. Saudação com Nome
+Objetivo: Declarar variáveis, usar concatenação e interpolação.
+Descrição: Crie um script que declare uma variável para o nome de
+uma pessoa e exiba uma saudação personalizada, por exemplo: "Olá,
+João! Bem-vindo ao nosso site!".
+Dica: Utilize tanto a concatenação (.) quanto a interpolação dentro de
+uma string com aspas duplas.*/
+string nome = "Kaique";
+Console.WriteLine($"Olá, {nome}! Bem-vindo ao nosso site!");
+Console.WriteLine("Olá " + nome + "! Bem-vindo ao nosso site!");
+/*2. Conversão de Temperatura (Celsius para Fahrenheit)
+Objetivo: Trabalhar com cálculos matemáticos e variáveis.
+Descrição: Crie um script que converta uma temperatura em Celsius
+para Fahrenheit. A fórmula para conversão é:
+Dica: Declare uma constante para armazenar o valor 9/5 e faça o
+cálculo com a variável fornecida. */
+const double divisao = 9.0 / 5.0;
+double c = 32;
+double resultado = (c * divisao) + 32;
+Console.WriteLine($"{c} graus Celsius, é igual a {resultado} Fahrenheit  ");
+
+/*3. Impressão de Dados do Usuário
+Objetivo: Concatenar e interpolar dados.
+Descrição: Crie um script que solicite ao usuário seu nome, idade e
+cidade e depois mostre uma mensagem com essas informações.
+Exemplo: "Meu nome é João, tenho 25 anos e moro em São Paulo".
+Dica: Use tanto concatenação quanto interpolação.*/
+string nomeUsuario = "Kaique";
+int idade = 23;
+string cidade = "São Paulo";
+Console.WriteLine($"Meu nome é {nomeUsuario}, tenho {idade} anos e moro em {cidade}");
+Console.WriteLine("Meu nome é " + nome + " , tenho " + idade + " anos e moro em " + cidade);
+
+/*4. Empréstimo Bancário
+Objetivo: Cálculos financeiros e declaração de variáveis.Descrição: Crie um script que calcule o valor de uma parcela de um
+empréstimo, dado o valor total do empréstimo, a taxa de juros e o
+número de parcelas. Exemplo de fórmula:
+Dica: Utilize variáveis para o valor do empréstimo, a taxa de juros e o
+número de parcelas.*/
+double valorEmprestimo = 7000;
+double taxaJuros = 0.10;
+int numeroParcelas = 5;
+double valorPorParcela = valorEmprestimo * (1 + taxaJuros) / numeroParcelas;
+Console.WriteLine($"O valor por parcela será igual a {valorPorParcela:F2}");
+/*5. Conversor de Moeda
+Objetivo: Trabalhar com variáveis, operadores e arredondamento.
+Descrição: Crie um script que converta uma quantia em reais para
+dólares, com base em uma taxa de câmbio fornecida. Mostre o valor
+convertido com 2 casas decimais.
+Dica: Use round() para arredondar o valor para 2 casas decimais.*/
+double taxaCambio = 5.32;
+double valorReais = 100;
+double valorDolares = valorReais / taxaCambio;
+valorDolares = Math.Round(valorDolares, 2);
+Console.WriteLine($"O valor de {valorReais} reais equivale a {valorDolares} dólares");
+/*6 . Calculadora de Desconto
+Objetivo: Cálculos matemáticos e uso de variáveis.
+Descrição: Crie um script que calcule o valor do desconto de um
+produto, dado o preço original e a porcentagem de desconto. Exemplo
+de fórmula:
+Dica: Use variáveis para armazenar o preço e a porcentagem do
+desconto e calcule o valor final.*/
+double precoOriginal = 800;
+double porcentagemDesconto = 20.0 / 100.0;
+double valorComDesconto = precoOriginal - (precoOriginal * porcentagemDesconto);
+Console.WriteLine($"O produto de preço {precoOriginal} com desconto sai por {valorComDesconto}");
